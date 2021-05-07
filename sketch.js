96c87a4b4e97a497050e6b5e477c3cf9ec5e127c
ronald-luo <r.luo.180@gmail.com>
2021-05-07 16:56:05 -0400

    creating UI

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ let w;
 let h;
 
 function setup() {
-    createCanvas(windowWidth,windowHeight);
-    w = floor(windowWidth/resolution)*0.8;
-    h = floor(windowHeight/resolution)*0.8;
+    createCanvas(800, 500);
+    w = floor(800 / resolution);
+    h = floor(500 / resolution);
     frameRate(9)
     snake = new Snake();
     foodlocation();
@@ -31,7 +31,7 @@ function draw() {
     snake.show();
 
     if (snake.endGame()) {
-        background (218, 66, 120);
+        background(218, 66, 120);
         noLoop();
     }
 
@@ -40,71 +40,71 @@ function draw() {
     rect(food.x, food.y, 1, 1);
 }
 
-
-
 function keyPressed() {
-    if (keyCode === UP_ARROW) {
-        snake.setDir(0,-1);
+    switch (keyCode) {
+        case UP_ARROW:
+            snake.setDir(0, -1);
+            break
+        case DOWN_ARROW:
+            snake.setDir(0, 1);
+            break
+        case RIGHT_ARROW:
+            snake.setDir(1, 0);
+            break
+        case LEFT_ARROW:
+            snake.setDir(-1, 0);
+            break
     }
-    else if (keyCode === DOWN_ARROW) {
-        snake.setDir(0,1);
-    }
-    else if (keyCode === RIGHT_ARROW) {
-        snake.setDir(1,0);
-    }
-    else if (keyCode === LEFT_ARROW) {
-        snake.setDir(-1,0);
-    }
-
 }
 
-document.addEventListener('touchstart', handleTouchStart, false);        
+
+document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
+var xDown = null;
 var yDown = null;
 
 function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
 
 function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
 
 function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
+    if (!xDown || !yDown) {
         return;
     }
 
-    var xUp = evt.touches[0].clientX;                                    
+    var xUp = evt.touches[0].clientX;
     var yUp = evt.touches[0].clientY;
 
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-            snake.setDir(-1,0);
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            /* left swipe */
+            snake.setDir(-1, 0);
         } else {
             /* right swipe */
-            snake.setDir(1,0);
-        }                       
+            snake.setDir(1, 0);
+        }
     } else {
-        if ( yDiff > 0 ) {
+        if (yDiff > 0) {
             /* up swipe */
-            snake.setDir(0,-1);
-        } else { 
+            snake.setDir(0, -1);
+        } else {
             /* down swipe */
-            snake.setDir(0,1);
-        }                                                                 
+            snake.setDir(0, 1);
+        }
     }
     /* reset values */
     xDown = null;
-    yDown = null;                                             
+    yDown = null;
 };
 
